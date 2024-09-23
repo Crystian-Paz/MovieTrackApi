bf4602057677bbb68710d505aeb9e8686eb0e869
Crystian-Paz <krlcrystian@gmail.com>
2024-09-23 15:38:11 -0300

    Adicionando o cors

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 import express from 'express';
 import filmsRouter from './Routes/filmsRouter.js';
 import musicsRouter from './Routes/musicsRouter.js';
+const cors = require('cors');
 
 const app = express();
 const port = 4000;
 
+app.use(cors({
+    origin: 'http://localhost:5173' // Substitua pela origem do seu frontend
+}));
+
 app.use(express.json());
 app.use(filmsRouter);
 app.use(musicsRouter);
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    } else {
+        next();
+    }
+});
 
 // Rotas principais
 app.get('/', (req, res) => {
